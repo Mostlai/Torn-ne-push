4e3b4e54a348b0966ef10b907617e449f4ff92ab
Ask sky <2096358571@qq.com>
2023-04-18 10:18:07 +0800

    0.0.3

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import math
 import os
 import requests
 import datetime
-
+import user_setting
 
 QMSG_KEY = os.environ["QMSG_KEY"]
 TORN_KEY = os.environ["TORN_KEY"]
-TORN_ID = '2783662'
+TORN_ID = user_setting.user_id
 webhook = 'https://qmsg.zendee.cn:443/send/' + QMSG_KEY
 api_url = 'https://api.torn.com/user/' + TORN_ID + '?selections=&key=' + TORN_KEY
 misc_url = 'https://api.torn.com/user/' + TORN_ID + '?selections=cooldowns,bars,newenvents,newmessages,refills&key=' + TORN_KEY
@@ -18,13 +18,13 @@ def analyse_second(second):
     m, s = divmod(second, 60)
     h, m = divmod(m, 60)
     return "%02d:%02d:%02d" % (h, m, s)
-    
+
 
 def events_reader(events_list):
-	event_str = '未读Events:'
-	for i in events_list:
-		event_str = events_str + i['event'] + '\n'
-	return event_str 
+    event_str = '未读Events:'
+    for i in events_list:
+        event_str = event_str + i['event'] + '\n'
+    return event_str
 
 
 def main_handler():
@@ -33,79 +33,75 @@ def main_handler():
     misc = requests.get(misc_url).json()
     name = data['name'] + '[{}]'.format(data['player_id'])
     status = '你还在城里面潇洒着'
-    more_info ='------\n'
-    
+    more_info = '------\n'
+
     if data["donator"] != 0:
-		energy_regen = 0.5
+        energy_regen = 0.5
 
     if data["status"]["state"] == 'Hospital':
-        status = '你看起来在医院潇洒 ' +  data["status"]["description"]
+        status = '你看起来在医院潇洒 ' + data["status"]["description"]
     if data["status"]["state"] == 'Jail':
-        status = '你看起来在监狱潇洒 ' +  data["status"]["description"]
+        status = '你看起来在监狱潇洒 ' + data["status"]["description"]
     if data["status"]["state"] == 'Traveling':
-        status = '你看起来在飞机潇洒 ' +  data["status"]["description"]
-        
+        status = '你看起来在飞机潇洒 ' + data["status"]["description"]
+
     event = events_reader(misc['events'])
-        
-    
+
     life = '{}/{}'.format(misc['life']['current'], misc['life']['maximum'])
     energy = '{}/{}'.format(misc['energy']['current'], misc['energy']['maximum'])
     nerve = '{}/{}'.format(misc['nerve']['current'], misc['nerve']['maximum'])
-    
 
     drug = '药物CD好了'
-    if misc['cooldowns']['drug']!=0:
+    if misc['cooldowns']['drug'] != 0:
         drug = '??药物CD: {}'.format(analyse_second(misc['cooldowns']['drug']))
-
 
     booster = '饮料CD好了'
     if misc['cooldowns']['booster'] != 0:
         booster = '??饮料CD: {}'.format(analyse_second(misc['cooldowns']['booster']))
 
-
     medical = '医药CD好了'
     if misc['cooldowns']['medical'] != 0:
         medical = '医药CD: {}'.format(analyse_second(misc['cooldowns']['medical']))
 
-
     if misc['energy']['current'] >= misc['energy']['maximum']:
         more_info += '能量满了哦'
     else:
-        achive_max_time = int(misc['energy']['maximum']) - int(misc['energy']['current'])/energy_regen
-        if achive_max_time>60:
-            achive_max_time=str(round(achive_max_time/60,1))+'小时'
+        achive_max_time = int(misc['energy']['maximum']) - int(misc['energy']['current']) / energy_regen
+        if achive_max_time > 60:
+            achive_max_time = str(round(achive_max_time / 60, 1)) + '小时'
         else:
-            achive_max_time=str(math.floor(achive_max_time))+'分钟'
+            achive_max_time = str(math.floor(achive_max_time)) + '分钟'
         more_info += '能量预计还有{}补满\n'.format(str(achive_max_time))
-
 
     if misc['nerve']['current'] >= misc['nerve']['maximum']:
         more_info += '勇气满了哦'
     else:
-        achive_max_time = int(misc['nerve']['maximum']) - int(misc['nerve']['current'])/nerve_regen
-        if achive_max_time>60:
-            achive_max_time=str(round(achive_max_time/60,1))+'小时'
+        achive_max_time = int(misc['nerve']['maximum']) - int(misc['nerve']['current']) / nerve_regen
+        if achive_max_time > 60:
+            achive_max_time = str(round(achive_max_time / 60, 1)) + '小时'
         else:
-            achive_max_time=str(math.floor(achive_max_time))+'分钟'
+            achive_max_time = str(math.floor(achive_max_time)) + '分钟'
         more_info += '勇气预计还有{}补满\n'.format(str(achive_max_time))
-        
-	
-	energy_refill = 'E Refill: '
-	if  misc['refills']['energy_refill_used'] == false:
-		energy_refill += '未使用'
-	else: energy_refill += '已使用'
-	nerve_refill = 'N Refill: '
-	if  misc['refills']['nerve_refill_used'] == false:
-		nerve_refill += '未使用'
-	else: nerve_refill += '已使用'
-	token_refill = 'T Refill: '
-	if  misc['refills']['token_refill_used'] == false:
-		token_refill += '未使用'
-	else: token_refill += '已使用'
 
+    energy_refill = 'E Refill: '
+    if not misc['refills']['energy_refill_used']:
+        energy_refill += '未使用'
+    else:
+        energy_refill += '已使用'
+    nerve_refill = 'N Refill: '
+    if not misc['refills']['nerve_refill_used']:
+        nerve_refill += '未使用'
+    else:
+        nerve_refill += '已使用'
+    token_refill = 'T Refill: '
+    if not misc['refills']['token_refill_used']:
+        token_refill += '未使用'
+    else:
+        token_refill += '已使用'
 
     message = '{}\n{}\n能量: {}\n勇气: {}\n生命: {}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(
-        name, status, energy, nerve, life, drug, booster, medical, more_info, energy_refill, nerve_refill, token_refill, events
+        name, status, energy, nerve, life, drug, booster, medical, more_info, energy_refill, nerve_refill, token_refill,
+        event
     )
     QQPusher(message)
 
